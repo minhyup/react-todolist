3c970d06d93e84e28c831ac2bcc939cb73904c39
minhyup <minhyupp@gmail.com>
2020-06-16 17:58:37 +0900

    'first'

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body{background: #e9fac8;}
+`;
+
+let test = 'fadfadf';
+
+function App() {
+  return (
+    <div>
+      <GlobalStyle />
+      <span>test</span>
+    </div>
+  );
+}
+
+export default App;
